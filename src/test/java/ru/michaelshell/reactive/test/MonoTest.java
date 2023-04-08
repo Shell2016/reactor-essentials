f783954d770cb5047c7057302c6025f1bbae851a
michaelshell <bvg2003@bk.ru>
2023-04-08 23:41:28 +0300

    Initial commit

---
+++ src/test/java/ru/michaelshell/reactive/test/MonoTest.java
@@ -0,0 +1,13 @@
+package ru.michaelshell.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void test1() {
+        log.info("Everything working...");
+    }
+}
